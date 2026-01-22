a10db89d7eabbac520d478e93327175053f54909
IzeD <issaclaikoiyit@gmail.com>
2026-01-22 21:18:05 +0700

    feat: copy result text

--- templator/lib/pages/apply_template_page.dart
+++ templator/lib/pages/apply_template_page.dart
@@ -1,8 +1,8 @@
-import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:templator/classes/field.dart';
 import 'package:templator/classes/template.dart';
-import 'package:templator/models/text_template_card.dart';
+import 'package:templator/models/single_instance/result_text_section.dart';
+import 'package:templator/models/single_instance/text_template_card.dart';
 
 class ApplyTemplatePage extends StatefulWidget {
   const ApplyTemplatePage({super.key, required this.title});
@@ -57,104 +57,6 @@ Rank/Name:
             ResultText(template: template),
             SizedBox(height: 200),
           ],
-        ),
-      ),
-    );
-  }
-}
-
-class ResultText extends StatefulWidget {
-  final Template template;
-
-  const ResultText({super.key, required this.template});
-
-  @override
-  State<ResultText> createState() => _ResultTextState();
-}
-
-class _ResultTextState extends State<ResultText> {
-  String _generatedText = "";
-
-  String _fillTemplate(Template template) {
-    String result = template.templateText;
-
-    log(
-      "applying field values to template (${template.name})",
-      name: "INFO",
-      level: 800
-    );
-    
-    for (var field in template.fields) {
-      log(
-        "reading Field: ${field.keyword}, value: ${field.value}",
-        name: "DEBUG (-v)",
-        level: 300
-      );
-
-      if (field.value == null) {
-        throw ArgumentError.notNull("Field: ${field.keyword}");
-      }
-
-      switch (field) {
-        case LabelledTextField f:
-          result = result.replaceAll('{{${f.keyword}}}', f.value!);
-          break;
-
-        case SelectionField f:
-          String joinedNames = f.value!.join("\n");
-          result = result.replaceAll('{{${field.keyword}}}', joinedNames);
-          break;
-      }
-    }
-
-    log("applied values to template, result:\n $result", name: "INFO", level: 800);
-    return result;
-  }
-
-  void _generateText() {
-    String templateResult;
-
-    log("user pressed generate text", name: "INFO", level: 800);
-
-    try {
-      templateResult = _fillTemplate(widget.template);
-    } catch (e) {
-      log("null value in fields!:", level: 900, error: e, name: "WARN");
-      templateResult = "please submit your fields.";
-    }
-
-    setState(() {
-      _generatedText = templateResult; 
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Container(
-        width: double.infinity,
-        color: Colors.grey[300],
-        padding: const EdgeInsets.all(8.0),
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Expanded(
-                child: Text(
-                  _generatedText.isEmpty
-                      ? "Click 'Generate Text' to fill in values"
-                      : _generatedText,
-                ),
-              ),
-              ElevatedButton(
-                onPressed: _generateText,
-                child: Text("Generate Text"),
-              ),
-            ],
-          ),
         ),
       ),
     );
